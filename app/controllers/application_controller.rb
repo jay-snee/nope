9138275fbf81478367f1b63dc011d5ca62173045
Jay Snee <jay.snee@me.com>
2018-07-14 11:49:30 +0100

    added after sign in/out controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,20 @@ class ApplicationController < ActionController::Base
     logger.info "CURRENT TENANT: #{Apartment::Tenant.current}"
   end
 
+  def after_sign_in_path_for(user_or_scope)
+    if Rails.env.development? || Rails.env.test?
+      "http://#{current_user.account.name}.localhost:#{request.port}"
+    else
+      "https://#{current_user.account.name}.#{ENV['APP_DOMAIN']}"
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    'http://www.profiler.datawrks.io'
+  end
+
+
+
   def validate_tenant
     redirect_to root_path unless current_user && Apartment::Tenant.current == current_user.account.name && !current_user.admin
   end
