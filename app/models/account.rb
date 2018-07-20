e5a8df832744e1405981ac08edade3053f21947d
Jay Snee <jay.snee@me.com>
2018-07-20 17:03:17 +0100

    removed apartment refs

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,13 +1,5 @@
 class Account < ApplicationRecord
 
-  after_create :create_tenant_database
-
   has_many :users, dependent: :destroy
-
-  private
-
-  def create_tenant_database
-    Apartment::Tenant.create(self.name)
-  end
 
 end
--- lib/rescued_apartment_middleware.rb
+++
@@ -1,9 +0,0 @@
-module RescuedApartmentMiddleware
-  def call(env)
-    begin
-      super
-    rescue Apartment::TenantNotFound
-      return [ 302, {'Location' =>"http://www.profiler.datawrks.io"}, [] ]
-    end
-  end
-end
