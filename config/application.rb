8d05684801558f93ebc59648b686b82bde27db34
Jay Snee <jay@arcane-apps.io>
2018-11-05 10:39:46 +0000

    Added Sendgrid SMTP config

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,15 @@ module Profiler
     config.generators.system_tests = nil
 
     config.active_job.queue_adapter = :sidekiq
+
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => ENV['SENDGRID_DOMAIN'],
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
