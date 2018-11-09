32bd1f1e91bcda79ddd33b6409733929b7d5f033
Jay Snee <jay@arcane-apps.io>
2018-11-09 16:42:22 +0000

    added User#send_to_websand

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,25 @@ class User < ApplicationRecord
                            foreign_key: :resource_owner_id,
                            dependent: :delete_all # or :destroy if you need callbacks
 
+  after_create :send_to_websand
+
   def tokens
     Doorkeeper::AccessToken.where(resource_owner_id: id).all
+  end
+
+  def send_to_websand
+    response = HTTParty.post(
+      'https://fair-custodian.websandhq.com/api/data/subscriber', 
+      {
+        'headers': {"Authorization": "Token #{ENV['WEBSAND_API_KEY']}"},
+        'body': {
+          "subscriber": {   
+            "email": email,   
+            "source": "Home Page",   
+            "subscribed_at": DateTime.now.iso8601
+          } 
+        }
+      })
   end
   
 end
