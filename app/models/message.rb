f0094254c6046f1f35b2f2bb1a198f4f8f48c18a
Jay Snee <jay@arcane-apps.io>
2018-11-23 10:16:04 +0000

    Added presence validation to Profile#name

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   belongs_to :profile
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, presence: true, allow_blank: false
 
   def parsed_html
     Nokogiri::HTML(html)
