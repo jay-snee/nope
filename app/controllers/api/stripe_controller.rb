c3ae693c1bb0f6f84b02b9066742b5112e27baff
Jay <jay@faircustodian.com>
2018-11-30 14:20:01 +0000

    skipping CSRF validation on stripe endpoint

--- app/controllers/api/stripe_controller.rb
+++ app/controllers/api/stripe_controller.rb
@@ -1,5 +1,7 @@
 class Api::StripeController < ApplicationController
 
+  skip_before_action :verify_authenticity_token, only: ['notifications']
+
   def notifications
     Notification.create(payload: params.to_json)
     render status: 200
