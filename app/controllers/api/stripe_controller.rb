065164ce3b45d1071d5de4412fce7c807245a91f
Jay <jay@faircustodian.com>
2018-11-30 14:07:19 +0000

    added routes

--- app/controllers/api/stripe_controller.rb
+++ app/controllers/api/stripe_controller.rb
@@ -2,7 +2,7 @@ class Api::StripeController < ApplicationController
 
   def notifications
     Notification.create(payload: params.to_json)
-    return status: 200
+    render status: 200
   end
 
 end
