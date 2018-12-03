7f5ded2985dc41246241642cc273559b6261ab2c
Jay <jay@faircustodian.com>
2018-12-03 07:50:23 +0000

    Added authentication across all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!
+
   private
 
   def verify_admin
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index]
+
   def index
     if current_user
       redirect_to action: 'dashboard'
