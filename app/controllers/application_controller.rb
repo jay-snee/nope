bdf48705111a4cd792bbd6cd7793a92a9465375f
Jay <jay@faircustodian.com>
2018-12-07 14:05:48 +0000

    cosmetics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def verify_admin
     redirect_to root_path unless current_user.admin
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:marketing_consent, :terms_consent])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_175026) do
+ActiveRecord::Schema.define(version: 2018_12_07_132131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,6 +149,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_175026) do
     t.string "stripe_customer_id"
     t.string "stripe_subscription_id"
     t.integer "max_profiles", default: 3
+    t.boolean "marketing_consent", default: false
+    t.boolean "terms_consent", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
