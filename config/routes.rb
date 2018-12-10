eb6c3f09df10762d563e56e6e24be90d28476b94
Jay <jay@faircustodian.com>
2018-12-10 16:19:28 +0000

    changed after signup redirect path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   namespace :api do
     post 'stripe/notifications'
