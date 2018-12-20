3dd120fcb142612183c91b16b70b4784de26552e
Jake Cowton <jakecowton@hotmail.com>
2018-12-20 23:21:41 +0000

    T&C JS modded for turbolinks

--- app/assets/javascripts/terms.js
+++ app/assets/javascripts/terms.js
@@ -1,14 +1,18 @@
-$(document).ready(function () {
-    $( "#user_terms_consent").change(function() {
-        if(document.getElementById("user_terms_consent").checked) {
-        sub = document.getElementsByName("commit")[0]
-        sub.disabled = false;
-        sub.classList.remove("disabled")
-      }
-      else  {
-        sub = document.getElementsByName("commit")[0]
-        sub.disabled = true;
-        sub.classList.add("disabled")
-      }
-    });
-});
+var ready;
+
+ready = function() {
+  return $('#user_terms_consent').change(function() {
+    var sub;
+    if (document.getElementById('user_terms_consent').checked) {
+      sub = document.getElementsByName('commit')[0];
+      sub.disabled = false;
+      sub.classList.remove('disabled');
+    } else {
+      sub = document.getElementsByName('commit')[0];
+      sub.disabled = true;
+      sub.classList.add('disabled');
+    }
+  });
+};
+
+$(document).on('turbolinks:load', ready);
