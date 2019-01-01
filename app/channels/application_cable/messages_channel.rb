b474483bbf745c8d7f8f6c4ce8eb715e9f80ba50
Jay <jay@faircustodian.com>
2019-01-01 12:28:10 +0000

    fixed channel base class

--- app/channels/application_cable/messages_channel.rb
+++ app/channels/application_cable/messages_channel.rb
@@ -1,4 +1,4 @@
-class ApplicationCable::MessagesChannel < Channel
+class ApplicationCable::MessagesChannel < ApplicationCable::Channel
   # Called when the consumer has successfully
   # become a subscriber to this channel.
   def subscribed
