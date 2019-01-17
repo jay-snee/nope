29844dd9cd39342c7be259705f14ed3a2d8dd86e
Jay <jay@faircustodian.com>
2019-01-17 15:30:36 +0000

    fixed referrals

--- app/jobs/users/referral_job.rb
+++ app/jobs/users/referral_job.rb
@@ -7,16 +7,16 @@ class Users::ReferralJob < ApplicationJob
     begin
       processed_code = referrer_code.downcase.delete(' ')
       user = User.find user_id
-  	  referrer = User.where(referral_code: referrer_code).first
+  	  referrer = User.where(referral_code: processed_code).first
 
   	  user.update(referred_by_id: referrer.id, max_profiles: (user.max_profiles + ENV["REFERRER_REWARD"].to_i))
 
   	  referrer.update(max_profiles: (referrer.max_profiles + ENV["REFERRER_REWARD"].to_i))
 
-  	  Processing::EventJob.perform_later("Referral Processed! Code: #{referrer_code} From: #{referrer.email}", "referral", true)
+  	  Processing::EventJob.perform_later("Referral Processed! Code: #{processed_code} From: #{referrer.email}", "referral", true)
     rescue
       user = User.find user_id
-      Processing::EventJob.perform_later("Referral processing failed: \n#{user.email}\n#{referrer_code}", "referral", true)
+      Processing::EventJob.perform_later("Referral processing failed: \n#{user.email}\n#{processed_code}", "referral", true)
     end
   end
 
