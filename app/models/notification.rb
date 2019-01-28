43ff1b4b874564a4af448211d0358a07146f9a69
Jay <jay@faircustodian.com>
2019-01-28 18:48:01 +0000

    refactored stripe notifications, added specs

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,33 +4,13 @@ class Notification < ApplicationRecord
 
   def process_payload
     data = JSON.parse payload
-
     case data['type']
-    
     when "customer.subscription.created"
-    
+      customer = User.where(stripe_customer_id: data['stripe']['data']['object']['customer']).first
+      customer.update(max_profiles: (customer.max_profiles + ENV["SUBSCRIPTION_MAX_PROFILE_COUNT"].to_i)) unless customer.nil?
     when "customer.subscription.deleted"
       customer = User.where(stripe_customer_id: data['stripe']['data']['object']['customer']).first
-      customer.update(max_profiles: ENV["SUBSCRIPTION_MIN_PROFILE_COUNT"], stripe_subscription_id: '')
-    
-    when "customer.created"
-    
-    when "customer.updated"
-    
-    when "customer.source.created"
-    
-    when "invoice.created"
-    
-    when "invoice.finalized"
-    
-    when "invoice.payment_succeeded"
-    
-    when "charge.succeeded"
-      customer = User.where(stripe_customer_id: data['stripe']['data']['object']['customer']).first
-      customer.update(max_profiles: (customer.max_profiles + ENV["SUBSCRIPTION_MAX_PROFILE_COUNT"]) unless customer.nil?
-    when "charge.failed"
-      customer = User.where(stripe_customer_id: data['stripe']['data']['object']['customer']).first
-      customer.update(max_profiles: ENV["SUBSCRIPTION_MIN_PROFILE_COUNT"]) unless customer.nil?
+      customer.update(max_profiles: (customer.max_profiles - ENV["SUBSCRIPTION_MAX_PROFILE_COUNT"].to_i), stripe_subscription_id: '')
     end
   end
 
--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -7,15 +7,27 @@ FactoryBot.define do
     password 			  {'password'}
     password_confirmation {'password'}
     confirmed_at 		  {Date.today}
+    max_profiles {3}
   end
 
-  factory :subscribed_user, class: User do
-    email 					{'subscribed_user@example.com'}
+  factory :unsubscribed_user, class: User do
+    email 					{'unsubscribed_user@example.com'}
     password 				{'password'}
     password_confirmation 	{'password'}
     confirmed_at 			{Date.today}
     stripe_customer_id 		{1234567}
+    stripe_subscription_id  {}
+    max_profiles {3}
+  end
+
+  factory :subscribed_user, class: User do
+    email           {'subscribed_user@example.com'}
+    password        {'password'}
+    password_confirmation   {'password'}
+    confirmed_at      {Date.today}
+    stripe_customer_id    {1234567}
     stripe_subscription_id  {1234567}
+    max_profiles {33}
   end
 
   factory :admin_user, class: User do
@@ -24,6 +36,7 @@ FactoryBot.define do
     password_confirmation {'password'}
     confirmed_at 		  {Date.today}
     admin 				  {true}
+    max_profiles {3}
   end
 
 end
