482fcd808f9438b426f8caa30587a61c8473610a
Jay <jay@faircustodian.com>
2019-03-06 08:19:17 +0000

    typo

--- app/jobs/users/referral_job.rb
+++ app/jobs/users/referral_job.rb
@@ -9,14 +9,14 @@ class Users::ReferralJob < ApplicationJob
       user = User.find user_id
 
       ref_code = ReferralCode.where(code: processed_code).first
-      ReferralCode.increment_count(:uses, ref_code.id, touch: true)
+      ReferralCode.increment_counter(:uses, ref_code.id, touch: true)
       referrer = ref_code.user
 
       user.update(
         referred_by_id: referrer.id, 
         max_profiles: (user.max_profiles + ENV["REFERRER_REWARD"].to_i + ENV["SUBSCRIPTION_MAX_PROFILE_COUNT"].to_i),
         trial: true,
-        trial_stared: DateTime.now
+        trial_started: DateTime.now
       )
       referrer.update(max_profiles: (referrer.max_profiles + ENV["REFERRER_REWARD"].to_i))
       Processing::EventJob.perform_later("Referral Processed! Code: #{processed_code} From: #{referrer.email}", "referral", true)
