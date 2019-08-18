90f0d8caa6b46ee3128192657b9497b0d2d68a2f
Jay Snee <jay@faircustodian.com>
2019-08-18 22:16:34 +0100

    parsing that json

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -23,8 +23,8 @@ class Api::DataController < ApiController
       end
 
       message = profile.user.messages.new(message_params.except(:html, :text, :spam_score, :attachments, :'attachment-info'))
-      message.html = message_params[:html].encode(message_params[:charsets][:html], 'UTF-8', invalid: :replace, undef: :replace, replace: "")
-      message.text = message_params[:text].encode(message_params[:charsets][:text], 'UTF-8', invalid: :replace, undef: :replace, replace: "")
+      message.html = message_params[:html].encode( JSON.parse(message.charsets)['html'], 'UTF-8', invalid: :replace, undef: :replace, replace: "")
+      message.text = message_params[:text].encode( JSON.parse(message.charsets)['text'], 'UTF-8', invalid: :replace, undef: :replace, replace: "")
       message.profile = profile
 
       if inbound_params[:attachments].to_i > 0
