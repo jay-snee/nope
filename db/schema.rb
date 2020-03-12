140b0d712e59aebff2b2d5ed0ffdecb3f96aafab
jay-snee <jay.snee@me.com>
2020-03-12 10:54:47 +0000

    removed Plans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_003454) do
+ActiveRecord::Schema.define(version: 2020_03_12_105403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,14 +107,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_003454) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
-  end
-
-  create_table "plans", force: :cascade do |t|
-    t.string "name"
-    t.string "stripe_id"
-    t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
